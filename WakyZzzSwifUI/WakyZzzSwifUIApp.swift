eab370263f263adc5fe7ac4a2183593230285914
Alex Ivanova <melnelen@gmail.com>
2024-06-25 17:32:05 +0200

    Initial Commit

---
+++ WakyZzzSwifUI/WakyZzzSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakyZzzSwifUIApp.swift
+//  WakyZzzSwifUI
+//
+//  Created by Alexandra Ivanova on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WakyZzzSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
